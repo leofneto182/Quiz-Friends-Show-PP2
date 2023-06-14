d9f19695bc8e7e91ec949dbafbc142d832dad4d3
Leonidio Neto <leonidio.freitas@outlook.com>
2023-06-15 00:55:11 +0100

    Add java script code

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,377 +1,187 @@
-/* Questions and answers for the quiz */
-
+// creating an array and passing the number, questions, options, and answers
 let questions = [
     {
-        numb: 1,
-        question: 'Where do Rachel and Ross go on their first date?',
-        answer: "Ross's museum",
-        options: [
-            'The cinema',
-            "Ross's museum",
-            'Central Perk',
-            'The Empire State Building'
-        ]
-    },
-
+      numb:  1,
+      question: 'Where do Rachel and Ross go on their first date?',
+      answer: "Ross's museum",
+      options: [
+          'The cinema',
+          "Ross's museum",
+          'Central Perk',
+          'The Empire State Building'
+    ]
+  },
     {
-        numb: 2,
-        question: 'Monica organises her towels into several categories. But how many?',
-        answer: '11',
-        options: [
-            '4',
-            '6',
-            '9',
-            '11'
-        ]
-    },
-
+      numb: 2,
+      question: 'Monica organises her towels into several categories. But how many?',
+      answer: '11',
+      options: [
+          '4',
+          '6',
+          '9',
+          '11'
+    ]
+  },
     {
-        numb: 3,
-        question: "What are Monica and Ross' parents called?",
-        answer: 'Jack and Judy',
-        options: [
-            'Jack and Judy',
-            'Jack and Jill',
-            'Peter and Margaret',
-            'Gerry and Mary'
-        ]
-    },
+      numb: 3,
+      question: "What are Monica and Ross' parents called?",
+      answer: 'Jack and Judy',
+      options: [
+          'Jack and Judy',
+          'Jack and Jill',
+          'Peter and Margaret',
+          'Gerry and Mary'
 
+    ]
+  },
     {
-        numb: 4,
-        question: "What's the first thing Ross says at the beginning of the opening episode?",
-        answer: '"Hi..."',
-        options: [
-            '"Has anyone seen my fossil collection?"',
-            '"How YOU doin?"',
-            '"Could I be wearing any more clothes?"',
-            '"Hi..."'
-        ]
-    },
+      numb: 4,
+      question: "What's the first thing Ross says at the beginning of the opening episode?",
+      answer: '"Hi..."',
+      options: [
+          '"Has anyone seen my fossil collection?"',
+          '"How YOU doin?"',
+          '"Could I be wearing any more clothes?"',
+          '"Hi..."'
 
+    ]
+  },
     {
-        numb: 5,
-        question: 'Which character says the last ever line in the series?',
-        answer: 'Chandler',
-        options: [
-            'Gunther',
-            'Phoebe',
-            'Chandler',
-            'Rachel'
-        ]
-    },
+      numb: 5,
+      question: 'Which character says the last ever line in the series?',
+      answer: 'Chandler',
+      options: [
+          'Gunther',
+          'Phoebe',
+          'Chandler',
+          'Rachel'
+    ]
+  },
 
-    {
-        numb: 6,
-        question: 'How many hours of Friends episodes are there to watch?',
-        answer: '90 and a bit',
-        options: [
-            '73 and a bit',
-            '84 and a bit',
-            '90 and a bit',
-            '112 exactly'
-        ]
-    },
+  {
+      numb: 6,
+      question: 'How many hours of Friends episodes are there to watch?',
+      answer: '90 and a bit',
+      options: [
+          '73 and a bit',
+          '84 and a bit',
+          '90 and a bit',
+          '112 exactly'
+      ]
+  },
 
-    {
-        numb: 7,
-        question: 'Chandler and Joey had two pets. What were they called?',
-        answer: 'Chick Jr. and Duck Jr.',
-        options: [
-            'Don and Ivana', 
-            'Brian and Larry', 
-            'Walter and Sergio', 
-            'Chick Jr. and Duck Jr.'
-        ]
-    },
+  {
+    numb: 7,
+    question: 'Chandler and Joey had two pets. What were they called?',
+    answer: 'Chick Jr. and Duck Jr.',
+    options: [
+        'Don and Ivana', 
+        'Brian and Larry', 
+        'Walter and Sergio', 
+        'Chick Jr. and Duck Jr.'
+    ]
+  },
 
-    {
-        numb: 8,
-        question: "What is the name of Joeys acting agent?",
-        answer: 'Estelle',
-        options: [
-            'Andrea',
-            'Estelle',
-            'Martha',
-            'Carole'
-        ]
-    },
+  {
+    numb: 8,
+    question: "What is the name of Joeys acting agent?",
+    answer: 'Estelle',
+    options: [
+        'Andrea',
+        'Estelle',
+        'Martha',
+        'Carole'
+      ]
+  },
+  
+  {
+    numb: 9,
+    question: 'What did Ross dress up as to teach Ben about Christmas and Hanukkah?',
+    answer: 'The Holiday Armadillo',
+    options: [
+        'The Holiday Aardvark',
+        'The Holiday Armadillo',
+        'The Holiday Turtle',
+        'The Holiday Tortoise'
+      ]
+  },
 
-    {
-        numb: 9,
-        question: 'What did Ross dress up as to teach Ben about Christmas and Hanukkah?',
-        answer: 'The Holiday Armadillo',
-        options: [
-            'The Holiday Aardvark',
-            'The Holiday Armadillo',
-            'The Holiday Turtle',
-            'The Holiday Tortoise'
-        ]
-    },
+  {
+    numb: 10,
+    question: 'Which job did Rachels fiancé Barry Farber do?',
+    answer: 'Orthodontist',
+    options: [
+        'Lawyer',
+        'Plumber',
+        'Orthodontist',
+        'Journalist'
 
-    {
-        numb: 10,
-        question: 'Which job did Rachels fiancé Barry Farber do?',
-        answer: 'Orthodontist',
-        options: [
-            'Lawyer',
-            'Plumber',
-            'Orthodontist',
-            'Journalist'
-        ]
-    },
+      ]
+  },
 
-    {
-        numb: 11,
-        question: "Chandler and Ross were a band while they were at college. Can you remember the band's name?",
-        answer: 'Way, No Way',
-        options: [
-            'Flock of Birds',
-            'Wyld Stallyns',
-            'Way, No Way', 
-            'Mosh Bros'
-        ]
-    },
+  {
+    numb: 11,
+    question: "Chandler and Ross were a band while they were at college. Can you remember the band's name?",
+    answer: 'Way, No Way',
+    options: [
+        'Flock of Birds',
+        'Wyld Stallyns',
+        'Way, No Way', 
+        'Mosh Bros'
 
-    {
-        numb: 12,
-        question: 'Who was the youngest person in the Friends gang?',
-        answer: 'Rachel',
-        options: [
-            'Rachel',
-            'Joey',
-            'Phoebe',
-            'Chandler'
-        ]
-    },
+      ]
+  },
 
-    {
-        numb: 13,
-        question: 'Apart from untidiness, what annoys Monica the most?',
-        answer: 'Animals dressed like humans',
-        options: [
-            'Cheesy garlic bread',
-            'Microwave popcorn',
-            'Canned soup',
-            'Animals dressed like humans'
-        ]
-    },
+  {
+    numb: 12,
+    question: 'Who was the youngest person in the Friends gang?',
+    answer: 'Rachel',
+    options: [
+        'Rachel',
+        'Joey',
+        'Phoebe',
+        'Chandler'
 
-    {
-        numb: 14,
-        question: 'Ross is allergic to which kind of fruit?',
-        answer: 'Kiwi'
-        options: [
-            'Lemon',
-            'Apple',
-            'Kiwi',
-            'Pomegranate'
-        ]
-    },
+      ]
+  },
 
-    {
-        numb: 15,
-        question: 'Phoebe dated a scientist called David. In which Belarus city did he work?',
-        answer: 'Minsk',
-        options: [
-            'Minsk',
-            'Grodno',
-            'Mogilev',
-            'Pinsk'
-        ]
-    },
+  {
+    numb: 13,
+    question: 'Apart from untidiness, what annoys Monica the most?',
+    answer: 'Animals dressed like humans',
+    options: [
+        'Cheesy garlic bread',
+        'Microwave popcorn',
+        'Canned soup',
+        'Animals dressed like humans'
 
-    {
-        numb: 16,
-        question: "When the Friends gang went to London for Ross' wedding, which member of the Royal Family did they bump into?",
-        answer: 'The Duchess of York, Sarah Ferguson',
-        options: [
-            'Prince Harry',
-            'Princess Anne',
-            'The Queen',
-            'The Duchess of York, Sarah Ferguson'
-        ]
-    },
+      ]
+  },
 
-    {
-        numb: 17,
-        question: "Ross had a son called Ben. What was Ben's first word?",
-        answer: 'Hi',
-        options: [
-            'Dinosaur',
-            'Coffee',
-            'Daddy',
-            'Hi'
-        ]
-    },
+  {
+    numb: 14,
+    question: 'Ross is allergic to which kind of fruit?',
+    answer: 'Kiwi',
+    options: [
+        'Lemon',
+        'Apple',
+        'Kiwi',
+        'Pomegranate'
+    ]
+  },
 
-    {
-        numb: 18,
-        question: "Monica dated a millionaire for a while. What was his name?",
-        answer: 'Pete Becker',
-        options: [
-            'Paul Becker',
-            'Jake Becker',
-            'Pete Becker',
-            'Boris Becker'
-        ]
-    },
+  {
+    numb: 15,
+    question: 'Phoebe dated a scientist called David. In which Belarus city did he work?',
+    answer: 'Minsk',
+    options: [
+        'Minsk',
+        'Grodno',
+        'Mogilev',
+        'Pinsk'
+      ]
+  }
 
-    {
-        numb: 19,
-        question: "Ross had a son called Ben. What was Ben's first word?",
-        answer: 'The Duchess of York, Sarah Ferguson',
-        options: [
-            'Dinosaur',
-            'Coffee',
-            'Daddy',
-            'Hi'
-        ]
-    },
+ 
 
-    {
-        numb: 20,
-        question: "Phoebe has a twin sister. What's her name?",
-        answer: 'Ursula',
-        options: [
-            'Scarlett',
-            'Ursula',
-            'Sharon',
-            'Rita'
-        ]
-    },
-
-    {
-        numb: 21,
-        question: "How many times has Ross been married?",
-        answer: 'Three times!',
-        options: [
-            'Just the once',
-            'Twice!',
-            'Three times!',
-            'Even the actor David Schwimmer has lost count!'
-        ]
-    },
-
-    {
-        numb: 22,
-        question: "What was the name of Joey's childhood imaginary friend?",
-        answer: 'Maurice',
-        options: [
-            'Mordecai',
-            'Murdoch',
-            'Maurice',
-            'Monty'
-        ]
-    },
-
-    {
-        numb: 22,
-        question: "Which of these is NOT one of the nicknames Rachel's parents call her?",
-        answer: 'Creampooft',
-        options: [
-            'Sweet pea',
-            'Pumpkin',
-            'Sweetie',
-            'Creampooft'
-        ]
-    },
-
-    {
-        numb: 23,
-        question: "How are Friends episode names written?",
-        answer: '"The one with___"',
-        options: [
-            '"The episode that includes____"',
-            '"That ____ episode!"',
-            '"The one with___"',
-            'In hieroglyphics'
-        ]
-    },
-
-    {
-        numb: 24,
-        question: "In which city is Friends set?",
-        answer: 'New York City',
-        options: [
-            'Los Angeles',
-            'New York City',
-            'Miami',
-            'Seattle'
-        ]
-    },
-
-    {
-        numb: 25,
-        question: "Who's worn a turkey on their head?",
-        answer: 'Monica',
-        options: [
-            'Monica',
-            'Phoebe',
-            'Chandler',
-            'Joey'
-        ]
-    },
-
-    {
-        numb: 26,
-        question: "What's the title of the Friends theme song?",
-        answer: "I'll Be There For You",
-        options: [
-            "I'll Be There For Them",
-            "I'll Be There For Those",
-            "I'll Be There For You",
-            "I'll Be There For That"
-        ]
-    },
-
-    {
-        numb: 27,
-        question: "What's the name of the coffee shop used by the characters?",
-        answer: 'Central Perk',
-        options: [
-            "St James' Perk",
-            'Central Perk',
-            'Froth Of Khan',
-            'Daily Grind'
-        ]
-    },
-
-    {
-        numb: 28,
-        question: "What instrument does Phoebe Buffay play?",
-        answer: 'Guitar',
-        options: [
-            'Drums',
-            'Guitar',
-            'Flute',
-            'Synthesiser'
-        ]
-    },
-
-    {
-        numb: 29,
-        question: "What hangs on Monica's purple door?",
-        answer: 'A yellow picture frame',
-        options: [
-            'A key hook',
-            'A coat hook',
-            'A yellow picture frame',
-            'A cap'
-        ]
-    },
-
-    {
-        numb: 30,
-        question: "What is the name of Phoebe's alter-ego?",
-        answer: 'Regina Falange',
-        options: [
-            'Phoebe Neeby',
-            'Monica Bing',
-            'Regina Falange',
-            'Elaine Benes'
-        ]
-    }
-
-    
-]
+];
